635e7c20e13274459b08b626b365e8c288103219
TimCrane1 <mickeymouth82@gmail.com>
2016-02-15 17:16:54 -0500

    FizzBuzz part 2

---
+++ FizzBuzz2.js
@@ -0,0 +1,31 @@
+var startOver = 1;
+while (startOver == 1) {
+   var userNumber = prompt("Choose a number!");
+   userNumber = +userNumber;
+   if (isNaN(userNumber)) {
+     alert("Make sure your entry is a number!");
+     startOver = 1;
+   }
+   else if (userNumber % 1 != 0) {
+     alert("Let's keep it simple, pick a number without a decimal.");
+     startOver = 1;
+   }
+   else {startOver = 0;
+   }
+}
+function fizzBuzz(number) {
+    for (var i = 1; i <= number; i++) {
+      if (i % 3 == 0 && i % 5 == 0) {
+         console.log("FizzBuzz");
+      }
+      else if (i % 3 == 0) {
+         console.log("Fizz");
+      }
+      else if (i % 5 == 0) {
+         console.log("Buzz");
+      }
+      else {console.log(i);
+      }
+    }
+}
+fizzBuzz(userNumber);
